300d902ba0bab99c91bf2da1784c54ca7bf333da
Compl Yue <compl.yue@icloud.com>
2020-04-18 00:38:36 +0800

    wip

--- edh/evt.py
+++ edh/evt.py
@@ -1,5 +1,5 @@
 """
-Event Processing
+Event processing constructs at par to Edh's
 
 """
 
@@ -10,48 +10,71 @@ from typing import *
 from .adt import *
 
 
-__all__ = ["BrdCstChan", "EventSink"]
+__all__ = ["Chan", "EventSink"]
 
 
-class BrdCstChan:
+class Chan:
     """
-    Broadcasting channel par to STM's see:
-        http://hackage.haskell.org/package/stm/docs/Control-Concurrent-STM-TChan.html#v:newBroadcastTChan
+    Chan here is some similar to STM's TChan, see:
+        http://hackage.haskell.org/package/stm/docs/Control-Concurrent-STM-TChan.html
 
-    But unlike with STM, such a chan can be read without dup
+    But unlike TChan of STM, a chan object here does NOT buffer items, when a Chan has
+    no reader, any item is immediately discarded when written to it.
 
-    While in Pythonic ways, such a chan can be used like:
-        async for ev in <chan>:
-            ...
+    And a Chan here can do both broadcasting as well as unicasting, even at the same
+    time, wrt how it is consumed:
+
+      *) Broadcast readers read a Chan with async for:
+            async for ev in chan:
+                ...
+         Multiple such concurrent readers are fed with the same stream of items
+
+      *) Unicast readers read a Chan by repeatedly call:
+            chan.read()
+         A stream of items are stochastically distributed to multiple such readers if
+         they run concurrently, in a respect load-balanced.
+
+      *) Mixing broadcast / unicast readers is techinically possible, but the semantic
+         is hard to reason, and seems barely purposeful.
 
     """
 
     __slots__ = "nxt"
 
-    def __init__(self):
-        loop = asyncio.get_running_loop()
-        self.nxt = loop.create_future()
+    def __init__(self, dupOf: Optional["Chan"] = None):
+        if dupOf is None:
+            loop = asyncio.get_running_loop()
+            self.nxt = loop.create_future()
+        elif isinstance(dupOf, Chan):
+            self.nxt = dupOf.nxt
+        else:
+            raise TypeError(f"Can not duplicate to a Chan from type {type(dupOf)!r}")
 
     async def write(self, ev):
         loop = asyncio.get_running_loop()
         nxt = loop.create_future()
-        self.nxt.set_result((ev, nxt))
+        if self.nxt.done():
+            # TODO triage what this situation is, should be dealt at all, then how ?
+            # may just be some Chans written separately after duplicated one or more
+            # from a common source, I don't see big problems so far.
+            pass
+        else:
+            self.nxt.set_result((ev, nxt))
         self.nxt = nxt
 
     async def read(self):
-        (ev, _) = await self.nxt
-        return ev
+        (itm, self.nxt) = await self.nxt
+        return itm
 
     async def __aiter__(self):
-        nxt = self.nxt
         while True:
-            (ev, nxt) = await nxt
-            yield ev
+            (itm, self.nxt) = await self.nxt
+            yield itm
 
 
 class EventSink:
     """
-    EventSink par to Edh's
+    EventSink at par to Edh's
 
     """
 
@@ -60,19 +83,20 @@ class EventSink:
     def __init__(self):
         self.seqn = 0
         self.mrv = None
-        self.chan = BrdCstChan()
+        self.chan = Chan()
         self.subc = 0
 
-    def subscribe(self) -> Tuple[BrdCstChan, Maybe[Any]]:
+    def subscribe(self) -> Tuple[Chan, Maybe[Any]]:
         """
-        Returns a readable channel and the most recent event value.
+        Returns a duplicated channel to read events, as well as the
+        most recent event value.
 
         """
         self.subc += 1  # todo simulate int64 wrap back?
         if self.seqn > 0:
-            return self.chan, Just(self.mrv)
+            return Chan(self.chan), Just(self.mrv)
         else:
-            return self.chan, Nothing()
+            return Chan(self.chan), Nothing()
 
     async def publish(self, ev):
         self.seqn += 1  # todo simulate int64 wrap back?
