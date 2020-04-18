f8a5723efc5cb9a476d1830477105d2ab5478ac8
Compl Yue <compl.yue@icloud.com>
2020-04-19 01:20:52 +0800

    fix fmt

--- proto/peer.py
+++ proto/peer.py
@@ -9,17 +9,14 @@ from typing import *
 
 from ..edh import *
 
+
 __all__ = ["CommCmd", "Peer"]
-
-
-CmdDir = str
-CmdSrc = str
 
 
 class CommCmd:
     __slots__ = ("dir", "src")
 
-    def __init__(self, src: CmdSrc, dir_: CmdDir = ""):
+    def __init__(self, src: str, dir_: str = ""):
         self.src = src
         self.dir = dir_
 
@@ -31,16 +28,17 @@ class Peer:
     def __init__(
         self,
         ident,
+        posting: Callable[[CommCmd], Awaitable[None]],
         hosting: Callable[[], Awaitable[CommCmd]],
         channels: Dict[Any, EventSink] = {},
-        posting:  Callable[[CommCmd],Awaitable[None]] ,
     ):
         self.ident = ident
+
         loop = asyncio.get_running_loop()
         self.eol = loop.create_future()
+        self.posting = posting
         self.hosting = hosting
         self.channels = channels
-        self.posting=posting
 
     def __repr__(self):
         return f"Peer<{self.ident}>"
